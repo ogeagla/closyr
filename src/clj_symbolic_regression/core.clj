ecec96c8ae89aa3fb2e27bd6888cff0b672a2709
Octavian Geagla <ogeagla@gmail.com>
2024-01-09 09:21:58 -0700

    wip eval exprs

--- src/clj_symbolic_regression/core.clj
+++ src/clj_symbolic_regression/core.clj
@@ -12,21 +12,55 @@
 
 ;; https://github.com/axkr/symja_android_library?tab=readme-ov-file#examples
 
+
+(defn expr->fn
+  [^ExprEvaluator util ^ISymbol variable ^IAST expr]
+  (F/Function (F/List (into-array ISymbol [variable]))
+              (.eval util expr)))
+
+
+(defn ->iexprs
+  [coll]
+  (into-array IExpr coll))
+
+
+(defn ->strings
+  [coll]
+  (into-array String coll))
+
+
 (defn demo-math
   []
 
-  (let [^ExprEvaluator util (ExprEvaluator. false 100)
+  (let [^ExprEvaluator util      (ExprEvaluator. false 100)
 
-        ^String java-form   (.toJavaForm util "D(sin(x)*cos(x),x)")
+        ^String java-form        (.toJavaForm util "D((sin(x)*cos(x))+5x,x)")
 
-        ^String x-str       "x"
-        _                   (println "DEBUG: "
-                                     (class F))
-        ^ISymbol x          (F/Dummy x-str)
-        ^IAST function      (F/D (F/Times (F/Sin x) (F/Cos x)) x)
-        ^IExpr result       (.eval util function)]
+        ^String x-str            "x"
+        ^ISymbol x               (F/Dummy x-str)
+        ^IAST function           (F/D (F/Times (F/Sin x) (F/Cos x)) x)
+        ^IAST function-b         (expr->fn util
+                                           x
+                                           (F/Plus
+                                             (->iexprs
+                                               [(F/C1)
+                                                (F/D (F/Times x (F/Times x x)) x)
+                                                (F/D (F/Times (F/Sin x) (F/Cos x)) x)])))
+
+        ^IExpr result-1          (.eval util function)
+        ^IExpr result-1-b        (.eval util function-b)
+        ^IExpr result-1-at-point (.evalFunction util function-b (->strings ["0.3"]))
+
+        ^IExpr result-2          (.eval util java-form)
+        ^IExpr result-3          (.eval util "diff(sin(x)*cos(x),x)")
+        ^IExpr result-4          (.eval util "integrate(sin(x)^5,x)")]
     (println "fn: " java-form)
-    (println "res1: " result)))
+    (println "res1: " (.toString result-1))
+    (println "res1b: " (.toString result-1-b))
+    (println "res1b-pt: " (.toString result-1-at-point))
+    (println "res2: " (.toString result-2))
+    (println "res3: " (.toString result-3))
+    (println "res4: " (.toString result-4))))
 
 
 (defn -main
