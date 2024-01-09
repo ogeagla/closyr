973ac4b300fa78c7ca9bf44c6698bde0ade20a63
Octavian Geagla <ogeagla@gmail.com>
2024-01-09 13:23:09 -0700

    wip GA

--- src/clj_symbolic_regression/ga.clj
+++ src/clj_symbolic_regression/ga.clj
@@ -8,12 +8,10 @@
    :mutation-fn  mutation-fn
    :crossover-fn crossover-fn})
 
-
 (defn evolve
   [{:keys [pop score-fn mutation-fn crossover-fn]}]
 
   (let [pop-shuff (shuffle pop)
-        ;; todo random sample step here?
         score*    (atom 0.0)
         new-pop   (loop [pop     pop-shuff
                          new-pop []]
@@ -22,17 +20,18 @@
                       (recur
                         (drop 2 pop)
                         (let [[e1 e2] [(first pop) (second pop)]
-                              new-new-pop (if (nil? e2)
-                                            (concat new-pop [e1])
-                                            (let [[s1 s2] [(score-fn e1) (score-fn e2)]
-                                                  better-e (if (>= s1 s2)
-                                                             e1 e2)
-                                                  new-e    (if (rand-nth [true false])
-                                                             (mutation-fn better-e)
-                                                             (crossover-fn better-e))]
-                                              (swap! score* + s1 (or s2 0.0))
-                                              (concat new-pop [better-e new-e])))]
-                          new-new-pop))))]
+                              newest-pop (if (nil? e2)
+                                           (do
+                                             (swap! score* + (score-fn e1))
+                                             (concat new-pop [e1]))
+                                           (let [[s1 s2] [(score-fn e1) (score-fn e2)]
+                                                 better-e (if (>= s1 s2) e1 e2)
+                                                 new-e    (if (rand-nth [true false])
+                                                            (mutation-fn better-e)
+                                                            (crossover-fn better-e))]
+                                             (swap! score* + s1 s2)
+                                             (concat new-pop [better-e new-e])))]
+                          newest-pop))))]
     {:pop           new-pop
      :pop-old       pop
      :score-fn      score-fn
@@ -41,7 +40,7 @@
      :crossover-fn  crossover-fn}))
 
 
-(def initial-pop [0 1 2 3])
+(def initial-pop [0 1 2 20 -1])
 
 
 (defn score-fn
