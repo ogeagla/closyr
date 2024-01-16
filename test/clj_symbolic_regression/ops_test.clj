f0915386bf858bf4f6b24877d0c02220d6cb5a8d
Octavian Geagla <ogeagla@gmail.com>
2024-01-16 16:03:26 -0700

    modify tests

---
+++ test/clj_symbolic_regression/ops_test.clj
@@ -0,0 +1,43 @@
+(ns clj-symbolic-regression.ops-test
+  (:require
+    [clj-symbolic-regression.ops :as ops]
+    [clojure.test :refer :all])
+  (:import
+    (org.matheclipse.core.expression
+      F)
+    (org.matheclipse.core.interfaces
+      IAST
+      IExpr
+      ISymbol)))
+
+
+(deftest modify-test
+  (testing "substitute"
+    (let [x (F/Dummy "x")]
+      (is (= (str (F/Sin x))
+             (str (:expr (ops/modify {:op           :substitute
+                                      :find-expr    F/Cos
+                                      :replace-expr F/Sin}
+                                     {:x-sym x
+                                      :expr  (F/Cos x)})))))))
+
+  (testing "fn"
+    (let [x (F/Dummy "x")]
+      (is (= (str (.plus x (F/Sin x)))
+             (str (:expr (ops/modify {:op          :fn
+                                      :modifier-fn (fn ^IExpr [{^IAST expr :expr ^ISymbol x-sym :sym :as pheno}]
+                                                     (F/Plus expr (F/Sin expr)))}
+                                     {:x-sym x
+                                      :expr  (.plus F/C0 x)})))))))
+
+  (testing "modify-leafs"
+    (let [x (F/Dummy "x")]
+      (is (= (str (F/Sin x))
+             (str (:expr (ops/modify {:op               :modify-leafs
+                                      :leaf-modifier-fn (fn ^IExpr [{^IAST expr :expr ^ISymbol x-sym :sym :as pheno} ^IExpr ie]
+                                                          (println "mod leaf " (str expr))
+                                                          (if (and (= (.toString ie) "x"))
+                                                            (F/Sin expr)
+                                                            ie))}
+                                     {:x-sym x
+                                      :expr  (.plus F/C0 x)}))))))))
