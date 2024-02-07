b87d8e65f997db9ac6461a2914607e4209111f4b
Octavian Geagla <ogeagla@gmail.com>
2024-02-06 18:47:43 -0700

    test

--- test/closyr/core_test.clj
+++ test/closyr/core_test.clj
@@ -4,6 +4,31 @@
     [closyr.core :refer :all]))
 
 
-(deftest a-test
-  (testing "FIXME"
-    (is (= 1 1))))
+(deftest cli-options-test
+  (testing "handles invalid inputs"
+    (is (=
+          (let [test-input '("-vvvp80000" "foo" "--help" "--invalid-opt" "-i50000" "-y1,1,1" "-x1,1,1,1,1,1,1,1,1")]
+            (validate-symreg-opts (parse-main-opts test-input)))
+
+          nil)))
+  (testing "handles valid short options"
+    (is (=
+          (let [test-input '("-t" "-p1000" "foo" "-i" "200" "-y" "1,2,30,4,5,6,10" "-x" "0,1,2,3,4,5,6")]
+            (validate-symreg-opts (parse-main-opts test-input)))
+
+          {:headless   true
+           :iterations 200
+           :population 1000
+           :xs         [0.0 1.0 2.0 3.0 4.0 5.0 6.0]
+           :ys         [1.0 2.0 30.0 4.0 5.0 6.0 10.0]})))
+
+  (testing "handles valid long options"
+    (is (=
+          (let [test-input '("--headless" "--population" "1000" "--iterations" "200" "--ys" "1,2,30,4,5,6,10" "--xs" "0,1,2,3,4,5,6")]
+            (validate-symreg-opts (parse-main-opts test-input)))
+
+          {:headless   true
+           :iterations 200
+           :population 1000
+           :xs         [0.0 1.0 2.0 3.0 4.0 5.0 6.0]
+           :ys         [1.0 2.0 30.0 4.0 5.0 6.0 10.0]}))))
