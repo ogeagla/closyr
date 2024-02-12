0dea6d5194f233302663b80bab81e6ef44a1a140
Octavian Geagla <ogeagla@gmail.com>
2024-02-12 13:44:21 -0700

    cli opts tests

--- test/closyr/symbolic_regression_test.clj
+++ test/closyr/symbolic_regression_test.clj
@@ -9,7 +9,7 @@
 
 
 (deftest can-run-from-cli-args
-  (testing "headless"
+  (testing "args from CLI are passed along correctly to implementation"
     (let [args* (atom nil)]
       (binding [ops/*print-top-n* 3]
         (is (=
@@ -39,7 +39,36 @@
                  :input-xs-count     3
                  :input-xs-vec       [0.0 1.0 2.0]
                  :input-ys-vec       [1.0 4.0 19.0]
-                 :max-leafs          20}]))))))
+                 :max-leafs          20}])))))
+
+
+  (testing "args from CLI are passed along correctly to implementation, and when none are passed we use defaults"
+    (let [args* (atom nil)]
+      (binding [ops/*print-top-n* 3]
+        (is (=
+              (with-redefs-fn {#'symreg/run-ga-iterations (fn [run-config run-args]
+                                                            (reset! args*
+                                                                    [(dissoc run-config :initial-muts :initial-phenos :input-xs-exprs :input-ys-exprs)
+                                                                     (dissoc run-args :extended-domain-args :initial-phenos :input-xs-list)])
+                                                            {:next-step :stop})
+                               #'symreg/config->log-steps (fn [_ _] 20)}
+                (fn []
+                  (symreg/run-app-from-cli-args
+                    {:population 30
+                     :iterations 20
+                     :headless   true})))
+              {:next-step :stop}))
+
+        (is (= @args*
+               [{:iters     20
+                 :max-leafs 40
+                 :use-gui?  false}
+                {:input-iters        20
+                 :input-phenos-count nil
+                 :input-xs-count     50
+                 :input-xs-vec       [0.0 0.20943951023931953 0.41887902047863906 0.6283185307179586 0.8377580409572781 1.0471975511965976 1.2566370614359172 1.4660765716752369 1.6755160819145563 1.8849555921538759 2.0943951023931953 2.3038346126325147 2.5132741228718345 2.7227136331111543 2.9321531433504737 3.141592653589793 3.3510321638291125 3.560471674068432 3.7699111843077517 3.979350694547071 4.1887902047863905 4.39822971502571 4.607669225265029 4.81710873550435 5.026548245743669 5.235987755982989 5.445427266222309 5.654866776461628 5.8643062867009474 6.073745796940266 6.283185307179586 6.492624817418906 6.702064327658225 6.911503837897546 7.120943348136864 7.3303828583761845 7.5398223686155035 7.749261878854823 7.958701389094142 8.168140899333462 8.377580409572781 8.587019919812102 8.79645943005142 9.00589894029074 9.215338450530059 9.42477796076938 9.6342174710087 9.843656981248019 10.053096491487338 10.262536001726657]
+                 :input-ys-vec       [0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0]
+                 :max-leafs          nil}]))))))
 
 
 (deftest can-run-experiment
