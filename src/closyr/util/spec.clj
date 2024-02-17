5ee04100b9854a09497a3e6e068770da6fa11b6e
Octavian Geagla <ogeagla@gmail.com>
2024-02-16 19:14:42 -0700

    WIP expr schema

--- src/closyr/util/spec.clj
+++ src/closyr/util/spec.clj
@@ -1,10 +1,18 @@
 (ns closyr.util.spec
   (:require
     [clojure.pprint :as pp]
+    [clojure.test.check.generators :as gen]
     [closyr.util.log :as log]
     [malli.core :as m]
     [malli.error :as me]
-    [malli.instrument :as mi]))
+    [malli.generator :as mg]
+    [malli.instrument :as mi]
+    [malli.transform :as mt])
+  (:import
+    (org.matheclipse.core.expression
+      F)
+    (org.matheclipse.core.interfaces
+      IExpr)))
 
 
 (def ^:dynamic *check-schema*
@@ -42,6 +50,49 @@
                                                'user]))]}))
 
 
+(def ^:private SymbolicExpr
+  (m/-simple-schema
+    {:type            :user/symbolic-expr
+     :pred            #(instance? IExpr %)
+     :type-properties {:error/fn      (fn [error _] (str "should be an IExpr, got " (:value error)))
+                       :error/message "should be IExpr"
+                       ;; :decode/string       mt/-string->long
+                       ;; :json-schema/type    "integer"
+                       ;; :json-schema/format  "int64"
+                       ;; :json-schema/minimum 6
+                       :gen/gen       (gen/let [f1 (gen/large-integer* {:min -1000 :max 1000})
+                                                f2 (gen/double* {:min -1000.0 :max 1000.0})]
+                                        (let [x (F/Dummy "x")]
+                                          (gen/elements
+                                            (mapv
+                                              (fn [^IExpr expr]
+                                                (F/Times (F/num (float f2))
+                                                         (F/Plus expr (F/num (float f1)))))
+                                              [x
+                                               (F/Sin x)
+                                               (F/Cos x)
+                                               (F/Exp x)
+                                               (F/Log x)
+                                               (F/Sqrt x)
+                                               (F/Times x F/C2)
+                                               (F/Plus x F/C5)
+                                               (F/Times x x)
+                                               (F/Times x (F/Times x x))]))))}}))
+
+
+(comment
+  (do
+    (println "--------")
+    (pp/pprint [:fn-expr-1 (m/validate SymbolicExpr (F/Dummy "x"))])
+    (pp/pprint [:fn-expr-2a (m/validate SymbolicExpr 123)])
+    (pp/pprint [:fn-expr-2b (m/validate SymbolicExpr F/C1)])
+    (pp/pprint [:fn-expr-3 (m/explain #'SymbolicExpr 123)])
+    (pp/pprint [:fn-expr-4 (me/humanize (m/explain #'SymbolicExpr 0))])
+    (pp/pprint [:fn-expr-5 (m/explain #'SymbolicExpr (F/Dummy "x"))])
+    (pp/pprint [:fn-expr-6 (str (mg/generate #'SymbolicExpr))])
+    (println "--------")))
+
+
 (def ^:private NumberVector
   [:vector number?])
 
@@ -51,7 +102,7 @@
    {:closed true}
    [:id {:optional true} :uuid]
    [:sym some?]
-   [:expr {:optional true} some?]
+   [:expr {:optional true} #'SymbolicExpr]
    [:score {:optional true} number?]
    [:util {:optional true} any?]
    [:last-op {:optional true} :string]
