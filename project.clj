73dbab5aa4ef5f8ed94fb911451411603bf239a5
Octavian Geagla <ogeagla@gmail.com>
2025-03-17 10:21:17 -0600

    bump deps; fix tests

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.async "1.6.681"]
-                 [metosin/malli "0.14.0"]
+                 [metosin/malli "0.17.0"]
 
                  [org.clojure/tools.cli "1.1.230"]
 
@@ -13,7 +13,7 @@
 
                  [seesaw/seesaw "1.5.0"]
                  [flames "0.5.0"]
-                 [org.knowm.xchart/xchart "3.8.7"]
+                 [org.knowm.xchart/xchart "3.8.8"]
                  ;; [com.github.weisj/darklaf-core "3.0.2"]
                  [io.github.vincenzopalazzo/material-ui-swing "1.1.4"]
                  [io.github.material-ui-swing/DarkStackOverflowTheme "0.0.1-rc3"]
@@ -44,8 +44,8 @@
             [lein-vanity "0.2.0"]
             [lein-nomis-ns-graph "0.14.6"]
             [lein-ancient "0.7.0"]
-            [jonase/eastwood "1.4.2"]
-            [lein-kibit "0.1.8"]
+            [jonase/eastwood "1.4.3"]
+            [lein-kibit "0.1.11"]
             [lein-bikeshed "0.5.2"]
             [venantius/yagni "0.1.7"]
             [lein-check-namespace-decls "1.0.4"]
